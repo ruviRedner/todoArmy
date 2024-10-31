25325ee8bd8759d68af851fc68cc3c71d66e82f9
ruviRedner <r.y.r7966@gmail.com>
2024-10-31 19:08:58 +0200

    delete and update done

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,31 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Todo } from "../App";
 import { deleteMission, updateMissionStatus } from "../servise/api-endpoints";
 interface Props {
   todo: Todo;
   setTodo: (todos: Todo[]) => void;
+  todos: Todo[];
   apikey: string;
 }
-const Item = ({ todo, setTodo,apikey }: Props) => {
+
+const Item = ({ todo, setTodo, apikey, todos }: Props) => {
   const handleDelete = async () => {
-    const del = await deleteMission(apikey,todo.id)
-    setTodo((prevTodos) => {prevTodos.filter((t) => t.id!== todo.id))
-    
-  
-  }
+    await deleteMission(apikey, todo._id!);
+    setTodo(todos.filter((t) => t._id !== todo._id));
+  };
   const hendelStatus = async () => {
-    const up = await updateMissionStatus(apikey,todo.id)
-  }
-  
+    const up = await updateMissionStatus(apikey, todo._id!);
+    setTodo(
+      todos.map((t) => (t._id === todo._id ? { ...t, status: up.status } : t))
+    );
+  };
+
   return (
-    <div className="item">
-      <h2>Name:{todo.name}</h2>
-      <p>Poirity:{todo.priority}</p>
-      <p>Status:{todo.status}</p>
-      <p>Description:{todo.description}</p>
+    <div
+      className="item"
+      style={{
+        backgroundColor:
+          todo.status === "Pending"
+            ? "red"
+            : todo.status === "In Progress"
+            ? "yellow"
+            : "green",
+      }}
+    >
+      <div className="y">
+        <h2>Name:{todo.name}</h2>
+        <p>Poirity:{todo.priority}</p>
+        <p>Status:{todo.status}</p>
+        <p>Description:{todo.description}</p>
+      </div>
       <div className="btn">
-      <button onClick={handleDelete}>Delete</button>
-      <button onClick={hendelStatus}>Pending</button>
+        <button className="del" onClick={handleDelete}>
+          Delete
+        </button>
+        <button
+          className="edit"
+          onClick={hendelStatus}
+          disabled={todo.status === "Completed"}
+        >
+          {todo.status === "Completed" ? "mission completed" : "progress"}
+        </button>
       </div>
     </div>
   );

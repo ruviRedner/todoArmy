376676f8f67cff1a7111fd74eda7e7478346ffa0
ruviRedner <r.y.r7966@gmail.com>
2024-10-31 19:09:31 +0200

    change useEffect

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,40 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import { Todo } from '../App'
-import Item from './Item';
-import { getMission } from '../servise/api-endpoints';
-interface Props{
-    todos:Todo[]
-    setTodos: (x:Todo[]) => void;
-    apikey: string
+import React, { useEffect, useState } from "react";
+import { Todo } from "../App";
+import Item from "./Item";
+import { getMission } from "../servise/api-endpoints";
+interface Props {
+  todos: Todo[];
+  setTodos: (x: Todo[]) => void;
+  apikey: string;
 }
-const List = ({todos,setTodos,apikey}:Props) => {
-  const [loading, setLoading] =useState<boolean>(true);
+const List = ({ todos, setTodos, apikey }: Props) => {
+  const [loading, setLoading] = useState<boolean>(true);
   useEffect(() => {
     const getMissions = async () => {
       try {
-        const res = await getMission(apikey)
-       
+        const res = await getMission(apikey);
 
         setTodos(res);
       } catch (error) {
         console.log(error);
-   
-        
-    }finally {
-      setLoading(false)
+      } finally {
+        setLoading(false);
+      }
     };
-  }
     getMissions();
-  },[]);
+  }, [todos]);
   return (
-    
-    <div className='list'>
-        <h1>Missions</h1>
-        {loading? <p>loading...</p> : null}
-        {todos.map((todo)=> <Item key={todo.name} todo={todo} setTodo={setTodos} apikey={apikey}/>)}
-      
+    <div className="list">
+      <h1>Missions</h1>
+      {loading ? <p>loading...</p> : null}
+      {todos.map((todo) => (
+        <Item
+          key={todo.name}
+          todo={todo}
+          setTodo={setTodos}
+          apikey={apikey}
+          todos={todos}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;

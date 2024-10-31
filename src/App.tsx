9565bb64a089279212793bdc4d90b66725ca2adc
ruviRedner <r.y.r7966@gmail.com>
2024-10-31 10:45:12 +0200

    app done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Add from './components/Add'
+import List from './components/List'
+export interface Todo{
+  name:string
+  status:string
+  priority:string
+  description:string
+}
 const App = () => {
+  const [todos,setTodo] = useState<Todo[]>([])
   return (
     <div>
-      
+      <h1>Management of military missions</h1>
+      <Add setTodo={setTodo}/>
+      <List todos={todos} setTodos={setTodo}/>
     </div>
   )
 }
